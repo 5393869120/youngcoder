ea13c7c486770d400467e12ab0d4d7eb911517db
ПристрійUNICEF <german.lupashko@perspektiva.dp.ua>
2024-12-03 17:43:57 +0200

    working

---
+++ src/figures/Triangle.java
@@ -0,0 +1,18 @@
+package figures;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+    public static void main(String[] args) {
+        Triangle triangle
+    }
+
+}
