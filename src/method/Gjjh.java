5f4093da9de6276d0a5095c352421760d7a63951
ПристрійUNICEF <german.lupashko@perspektiva.dp.ua>
2024-12-17 17:32:04 +0200

    myMet

---
+++ src/method/Gjjh.java
@@ -0,0 +1,14 @@
+package method;
+
+public class  Gjjh {
+    static void myMethod() {
+        System.out.println( "you bro " );
+    }
+        public static void main(String[] args)  {
+            myMethod();
+            myMethod();
+            myMethod();
+    }
+}
+
+
